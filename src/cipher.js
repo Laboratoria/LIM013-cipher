a1e1ff1342beec4c06b7026d8d556ccbd1ea832a
LauraJimenezB <laura271260@gmail.com>
2020-07-16 12:07:04 -0500

    actualizar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
+  let offset = document.getElementById('numero').value;
+  let str = document.getElementById('text').value;
+  let newStr = '';
+
+
+  encode = function (offset, str) {
+      for (let i=0; i<str; i++) {
+        let currentLetter = str [i];
+        if (currentLetter==='') {
+          newString += currentLetter;
+          continue;
+        let convert = [(str.charCodeAt (0))-65+ offset]%26 +65
+        let result = str.fromCharCode(convert);
+        }
+      }
+    }
+
+  decode = function (offset, str) {
+    let deconvert = [(str.charCodeAt (0))-65- offset]%26 +65
+    let result = str.fromCharCode(deconvert);
+    }
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+codificar.addEventListener ("click", encode());
+
+function encode () {
+  alert ("Hola");
+}
