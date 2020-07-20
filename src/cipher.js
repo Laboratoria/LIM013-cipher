ce0f96cea105a18263780a8ea4e247fa7b526e4b
Daniela-delacruz <a.danieladelacruz@gmail.com>
2020-07-20 11:28:25 -0500

    Creanoo variables

--- src/cipher.js
+++ src/cipher.js
@@ -2,10 +2,18 @@
  
 };
 export default cipher;*/
-
+//estructura de objetos, que va a guardar encode y decode
 const cipher = {
  
 };
 
 export default cipher;
-alert("hola")
+
+//- `cipher.encode(offset, string)`: `offset` es el número de posiciones que
+//queremos mover a la derecha en el alfabeto y `string` el mensaje (texto)
+//que queremos cifrar
+//- `cipher.decode(offset, string)`: `offset` es el número de posiciones que
+//queremos mover a la izquierda en el alfabeto y `string` el mensaje
+//(texto) que queremos descifrar.
+//colocar su funcionalidad disponible para todo el proyecto, traes la funcionalidad al index.js
+
